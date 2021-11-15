6db78cb1c02560cf30e077f7d93af131e15db9a3
bigbigbig <krisada.m@ku.th>
2021-11-15 15:44:39 +0700

    edited css index.html

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,97 +1,102 @@
 @extends('layouts.main')
 
 @section('content')
-
-
-
-
-    <div class=" mt-5 border-4 border-light-blue-500 border-opacity-50 w-1/2 mx-auto">
+    <div class=" mt-5 w-full md:w-5/6 lg:w-3/4 mx-auto">
         <div class="z-10 " id="cover" onfocus="">
-
-            <div class="bg-black text-white pl-3">
-                postcard
-            </div>
-            <div class="my-8">
-                <img src="{{ $imgUrl }}" alt="Bird1" class="object-contain w-full" style="height: 500px">
-            </div>
-            <form action="{{ route('send-email') }}" method="post">
-                @csrf
-                <div class="">
-                    <div>
-                        <input class="hidden  " type="text" name="img-url" value="{{ $imgUrl }}">
-                    </div>
-                    <hr>
-                    <div class="mb-2" id="" onclick="">
-                        <input class="w-full text-xl pl-3 " type="text" name="senderName" placeholder="ผู้ส่ง"
-                            autocomplete="off">
-                    </div>
-                    @error('senderName')
+            <h1 class="bg-black text-white text-5xl py-4 pl-3 w-full">
+                โปสการ์ด
+            </h1>
+            <div class="mb-28 ">
+                <div class="bg-gray-100 border-r-4 border-l-4 border-gray-300 pt-8">
+                    <img src="{{ $imgUrl }}" alt="Bird1" class="object-contain mx-auto max-w-full shadow-lg"
+                        style="max-height: 500px">
+                </div>
+                <form action="{{ route('send-email') }}" method="post">
+                    @csrf
+                    <div
+                        class="border-r-4 border-b-4 border-l-4 bg-gray-100 border-gray-300 px-4 py-6 text-md md:text-xl space-y-3">
                         <div>
-                            <span class="text-red-600">{{ $message }}</span>
+                            <input class="hidden  " type="text" name="img-url" value="{{ $imgUrl }}">
                         </div>
-                    @enderror
-                    <div class="hidden mb-2" id="sendDiv" onclick="">
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <div>
-                                            <span>ถึง</span>
-                                        </div>
-                                    </td>
-                                    <td class="w-full">
-                                        <div class="">
-
-                                            <input id="toEmail" class="w-full pl-2" type="text" name="toEmail"
-                                                onblur="showDiv()">
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="mb-2" id="first" onclick="showSendDiv()">
-                        <input class="w-full text-xl  pl-3" type="text" placeholder="ผู้รับ" autocomplete="off">
-                        @error('toEmail')
-                            <div>
-                                <span class="text-red-600">{{ $message }}</span>
-                            </div>
-                        @enderror
-                    </div>
-                    <hr>
-                    <div>
-                        <input class="w-full text-xl   pl-3" type="text" name="receiveName" placeholder="ชื่อผู้รับ"
-                            autocomplete="off">
-                        @error('receiveName')
-                            <div>
-                                <span class="text-red-600">{{ $message }}</span>
-                            </div>
-                        @enderror
-                    </div>
-                    <hr>
-                    <div class="relative">
-                        <textarea id="card_content" name="body" class=" w-full relative text-xl block pl-3" maxlength="100"
-                            autocomplete="off" onkeydown="inputSize()" onkeyup="inputSize()"></textarea>
-                        <div id="content_size" class="absolute right-0">
-                            0/100
+                        <hr>
+                        <div class="mb-2 space-y-3" id="" onclick="">
+                            <label for="senderName">ชื่อผู้ส่ง</label>
+                            <input class="w-full text-xl pl-3 py-2" type="text" name="senderName" placeholder="ผู้ส่ง"
+                                autocomplete="off">
                         </div>
-                        @error('body')
+                        @error('senderName')
                             <div>
                                 <span class="text-red-600">{{ $message }}</span>
                             </div>
                         @enderror
+                        <div class="hidden mb-2 space-y-3" id="sendDiv" onclick="">
+                            <label for="receiverEmail">อีเมลผู้รับ</label>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <div>
+                                                <span>ถึง</span>
+                                            </div>
+                                        </td>
+                                        <td class="w-full">
+                                            <div class="">
+                                                <input id="toEmail" class="w-full  py-2 pl-2" type="email" name="toEmail"
+                                                    onblur="showDiv()">
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="mb-2 space-y-3 " id="first" onclick="showSendDiv()">
+                            <label for="receiverEmail">อีเมลผู้รับ</label>
+                            <h1 class="bg-gray-50 text-gray-400 px-3 py-2 font-medium text-xl ">ผู้รับ</h1>
+                            @error('toEmail')
+                                <div>
+                                    <span class="text-red-600">{{ $message }}</span>
+                                </div>
+                            @enderror
+                        </div>
+                        <hr>
+                        <div class="space-y-3">
+                            <label for="receiverName">ชื่อผู้รับ</label>
+                            <input class="w-full text-xl py-2 pl-3" type="text" name="receiveName" placeholder="ชื่อผู้รับ"
+                                autocomplete="off">
+                            @error('receiveName')
+                                <div>
+                                    <span class="text-red-600">{{ $message }}</span>
+                                </div>
+                            @enderror
+                        </div>
+                        <hr>
+                        <div class="relative space-y-3">
+                            <label for="content">เนื้อหา</label>
+                            <textarea id="card_content" name="body" class=" w-full relative text-xl block pl-3"
+                                maxlength="100" autocomplete="off" onkeydown="inputSize()" onkeyup="inputSize()"></textarea>
+                            <div id="content_size" class="absolute right-0">
+                                0/100
+                            </div>
+                            @error('body')
+                                <div>
+                                    <span class="text-red-600">{{ $message }}</span>
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="text-center ">
+                            <button type="submit"
+                                class="bg-blue-500 mt-8 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded ">ส่งโปสการ์ด</button>
+                        </div>
+                        <div class="text-right">
+                            <a rel="canonical" href="https://twitter.com/intent/tweet" data-show-count="false"
+                                data-size="large" data-via="KUredcross" data-text="post card">Share
+                                via
+                                Twitter</a>
+                        </div>
                     </div>
-
-                </div>
-                <button type="submit"
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sent</button>
-                <div id="tweet-button">
-                    <a rel="canonical" href="https://twitter.com/intent/tweet" class="twitter-share-button"
-                        data-show-count="false" data-size="large" data-via="KUredcross" data-text="post card">Tweet</a>
-
-                </div>
-
-            </form>
+                </form>
+            </div>
         </div>
     </div>
 
@@ -119,4 +124,5 @@
             '<a rel="canonical" href="https://twitter.com/intent/tweet" class="twitter-share-button" data-size="large" data-via="KUredcross " data-show-count="false" data-url="http://6749-1-46-138-111.ngrok.io/bird/2" data-text="test text" >Tweet</a>'
         twttr.widgets.load()
     </script>
+
 @endsection
