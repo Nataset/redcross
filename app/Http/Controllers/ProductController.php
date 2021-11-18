1f1982bd3a43e7ed7bfb0ee1bc75975e2e584db2
Chris-Ch-R <chayangkun.r@ku.th>
2021-11-18 15:06:10 +0700

    editUrl

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 
@@ -30,5 +31,18 @@ class ProductController extends Controller
 
         return redirect()->away('https://www.google.com');
         // return redirect()->away($product->product_url);
+    }
+
+    public function editUrl(Request $request , $id)
+    {
+        $product = Product::findOrFail( $id );
+
+        if($request->product_url !== null){
+            $product->product_url = $request->product_url;
+        }
+
+        $product->save();
+        return redirect()->intended(RouteServiceProvider::HOME)->with("message" , "edited");
+
     }
 }
