84a81e7a59164c8439d3ea28d22f84d30247169b
bigbigbig <krisada.m@ku.th>
2021-11-18 16:52:29 +0700

    added css to admin panel

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,46 +1,58 @@
 <x-app-layout>
-    {{-- @if (session()->exists('message')) --}}
-    {{-- <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show"> --}}
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    {{ session('message') }}
+    <x-slot name="status">
+        @if (session()->exists('message'))
+            <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show">
+                <div class="py-12">
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-6 bg-white border-b border-gray-200">
+                                {{ session('message') }}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                {{-- </div> --}}
             </div>
-        </div>
-    </div>
-    {{-- @endif --}}
-    <x-slot name="header">
+        @endif
+    </x-slot>
+    {{-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Product') }}
         </h2>
-    </x-slot>
+    </x-slot> --}}
     <x-slot name="slot">
-        <table>
-            <thead class="border">
-                <th class="border">
-                    Name
+        <table class="mx-auto my-14 shadow-xl">
+            <thead class=" text-2xl">
+                <th class="border-r-2 py-4 px-8  border-gray-600">
+                    No.
                 </th>
-                <th class="border">
-                    URL
+                <th class="border-r-2 py-4 px-8  border-gray-600">
+                    Product's Name
                 </th>
-                <th class="border">
+                <th class="border-r-2 py-4 px-8 border-gray-600">
+                    Product's URL
+                </th>
+                <th class="border-r-2 border-gray-600 py-4 px-8">
                     Number of clicks
                 </th>
             </thead>
             <tbody>
                 @foreach ($products as $product)
-                    <tr class="border">
-                        <td class="border">
+                    <tr class="border-2 border-gray-600 text-xl">
+                        <td class="border-2 py-2 px-4 border-gray-600">
+                            {{ $loop->index + 1 }}
+                        </td>
+                        <td class="border-2 border-gray-600 py-2 px-4">
                             {{ $product->product_name }}
                         </td>
-                        <td class="border">
+                        <td class="border-2 border-gray-600 py-2 px-4">
                             {{ $product->product_url }}
                         </td>
-                        <td class="border">
+                        <td class="border-2 py-2 px-4 border-gray-600 text-center">
                             {{ $product->click_amount }}
+                        </td>
+                        <td class="border-2 py-3 px-4 border-gray-600">
+                            <a href="{{ url('/edit/product/' . $product->id) }} "
+                                class="bg-yellow-400 px-6 py-1 rounded-lg font-bold mx-2 my-2 hover:bg-yellow-200 hover:text-gray-900  ">Edit</a>
                         </td>
                     </tr>
                 @endforeach
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,39 +1,36 @@
 @extends('layouts.main')
 @section('content')
-<fieldset>
-    <legend>Edit Product</legend>
-<form action="{{route('confirmEditProduct' , ['id' => $product->id] )}}" method="post">
-    @csrf
-    @method('PUT')
-    <div>
-        <label for="name">Product name</label>
-        <input type="text" name="name"
-        autocomplete="off" placeholder="name"
-        value="{{$product->name}}">
-        @error('name')
-        <div style="color:red">
-            {{ $message }}
-        </div>
-        @enderror
-    </div>
-    <div>
-        <label for="url">Product url</label>
-        <input type="text" name="url"
-        autocomplete="off" placeholder="url"
-        value="{{$product->url}}">
-    </div>
-    @error('url')
-        <div style="color:red">
-            {{ $message }}
-        </div>
-    @enderror
+    <fieldset class="m-20 border-2 pl-3 border-gray-600">
+        <legend class="text-3xl font-semibold"> Edit Product </legend>
+        <form action="{{ route('confirmEditProduct', ['id' => $product->id]) }}" method="post"
+            class="mx-auto px-6  space-y-6">
+            @csrf
+            @method('PUT')
+            <div class="text-xl">
+                <label for="name">Product name : </label>
+                <input type="text" name="name" autocomplete="off" placeholder="name" class="rounded-lg"
+                    value="{{ old('name', $product->product_name) }}">
+                @error('name')
+                    <div style="color:red">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+            <div class="text-xl">
+                <label for="url">Product url : </label>
+                <input type="text" name="url" autocomplete="off" placeholder="url" class="rounded-lg"
+                    value="{{ old('url', $product->product_url) }}">
+            </div>
+            @error('url')
+                <div style="color:red">
+                    {{ $message }}
+                </div>
+            @enderror
 
-    <div>
-        <button type="submit">Edit Product</button>
-    </div>
-
-
-
-</form>
-</fieldset>
-@endsection
+            <div class="text-xl">
+                <button type="submit" onclick="return confirm('ยืนยันการเปลี่ยนแปลง')"
+                    class="bg-blue-500 mb-4  hover:bg-blue-700 text-white font-bold py-2 px-8 rounded "> Edit
+                    Product</button>
+            </div>
+        </form>
+</fieldset> @endsection
